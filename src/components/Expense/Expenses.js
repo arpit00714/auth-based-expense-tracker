241ec25219134fff018b493ff4f801f04c96f49b
arpit00714 <arpitkhare14@gmail.com>
2023-04-29 14:00:35 +0530

    edit and delete expenses

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import ExpenseForm from "./ExpenseForm";
+import UpdateForm from "./UpdateForm";
 
 function Expenses() {
   // const [expenses, setExpenses] = useState([
@@ -11,6 +12,10 @@ function Expenses() {
     "https://ecommerce-website-9cea4-default-rtdb.firebaseio.com/expenses.json";
 
   const [expenses, setExpenses] = useState([]);
+  const [show, setShow] = useState(false);
+  const [id, setId] = useState("");
+  const [data, setData] = useState({});
+
   useEffect(() => {
     getExpenses();
   }, []);
@@ -32,7 +37,19 @@ function Expenses() {
       });
   };
 
-  const onAdd = async (expense) => {
+  const getData = (id) => {
+    fetch(
+      `https://fir-99cf8-default-rtdb.asia-southeast1.firebasedatabase.app/expenses/${id}.json`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setId(id);
+        setData(data);
+        setShow((prev) => !prev);
+      });
+  };
+
+  const addExpense = async (expense) => {
     const options = {
       method: "POST",
       body: JSON.stringify(expense),
@@ -47,15 +64,52 @@ function Expenses() {
       const res = await fetch(url, options);
       const data = await res.json();
       // getExpenses()
-      setExpenses([...expenses, expense]);
+      //   setExpenses([...expenses, expense]);
+      console.log(data);
+
+      getExpenses();
     } catch (error) {
       console.error(error);
     }
   };
 
+  const deleteExpense = (id) => {
+    fetch(
+      `https://fir-99cf8-default-rtdb.asia-southeast1.firebasedatabase.app/expenses/${id}.json`,
+      {
+        method: "DELETE",
+      }
+    ).then(() => {
+      console.log("Expense successfuly deleted");
+    });
+    const filteredExpenses = expenses.filter((expense) => expense.id != id);
+    setExpenses(filteredExpenses);
+  };
+
+  const editExpense = (expense, id) => {
+    fetch(
+      `https://fir-99cf8-default-rtdb.asia-southeast1.firebasedatabase.app/expenses/${id}.json`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(expense),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((data) => {
+      console.log(data);
+      getExpenses();
+    });
+  };
+
   return (
     <>
-      <ExpenseForm addExpense={onAdd} />
+      {/* <ExpenseForm addExpense={onAdd} /> */}
+      {!show ? (
+        <ExpenseForm addExpense={addExpense} />
+      ) : (
+        <UpdateForm edit={editExpense} data={data} setShow={setShow} id={id} />
+      )}
       <h1
         style={{
           textTransform: "uppercase",
@@ -66,13 +120,25 @@ function Expenses() {
         Your Expenses
       </h1>
       <section className="expenses">
-        {expenses.map((item) => (
-          <article className="expense" key={item.description}>
-            <h2>{item.description}</h2>
-            <h3>{item.amount}</h3>
-            <p>{item.category}</p>
-          </article>
-        ))}
+        {expenses.length > 0 &&
+          expenses.map((item) => (
+            <article className="expense" key={item.id}>
+              <h2>{item.description}</h2>
+              <h3>{item.amount}</h3>
+              <p>{item.category}</p>
+              <div>
+                <button className="edit" onClick={() => getData(item.id)}>
+                  Edit
+                </button>
+                <button
+                  className="delete"
+                  onClick={() => deleteExpense(item.id)}
+                >
+                  Delete
+                </button>
+              </div>
+            </article>
+          ))}
       </section>
     </>
   );
